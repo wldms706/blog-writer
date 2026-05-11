39abc17c52355876f5b632e40095ebb4596393b9
지은이 <jieun-@jieunsui-MacBookPro.local>
2026-05-11 15:08:39 +0900

    Google Analytics 4 (G-JYT0FEXTVS) 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,22 @@ export default function RootLayout({
           `,
         }}
       />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-JYT0FEXTVS"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JYT0FEXTVS');
+          `,
+        }}
+      />
       <body className="antialiased">
         {children}
         <KakaoChatButton />
